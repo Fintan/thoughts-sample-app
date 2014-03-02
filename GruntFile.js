0a2a7b4b7ffa69c3b6f8af88f55024c1dd3754b3
Fintan Boyle <fintanboyle@gmail.com>
2014-03-02 16:06:34 +0000

    upgraded to latest Backbone
    I decided to upgrade to latest Backbone because router now has an
    'execute' method.  Backbone is now a proper AMD module and had to also
    upgrade require.js to accomodate the shim for it.
    grunt-contrib-requirejs plugin dependencies also need an 'npm install'
    to pull down latest requirejs

--- GruntFile.js
+++ GruntFile.js
@@ -17,6 +17,7 @@ module.exports = function (grunt) {
         preserveLicenseComments: false,
         useStrict: true,
         wrap: true,
+        wrapShim: true, 
         done:function(done, output) {
             grunt.task.run(['copy:require_to_dist']);
             done();
